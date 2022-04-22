2ee06454738a7863bedba176591352966a412f2f
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-23 02:32:48 +0600

    delete api add

--- index.js
+++ index.js
@@ -38,6 +38,13 @@ async function run() {
       const result = await serviceCollection.insertOne(newService);
       res.send(result);
     });
+
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
